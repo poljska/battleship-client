86f005696b49f4fa2cf510fb0299e8391632bf2b
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-04-30 00:15:51 +0200

    Added game data download

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     authToken: "",
-    player: ""
+    player: "",
+    game: {}
   },
   mutations: {
     setAuthToken(state, authToken) {
@@ -14,10 +15,16 @@ export default new Vuex.Store({
     },
     setPlayer(state, player) {
       state.player = player;
+    },
+    setGameId(state, gameId) {
+      state.game.game_id = gameId;
+    },
+    setGame(state, game) {
+      state.game = game;
     }
   },
   actions: {
-    createNewGame() {
+    createNewGame({ commit, dispatch }) {
       let request = new XMLHttpRequest();
       request.open("POST", "https://battleship-server-php.herokuapp.com/games");
       request.addEventListener("readystatechange", () => {
@@ -25,13 +32,14 @@ export default new Vuex.Store({
           request.readyState == XMLHttpRequest.DONE &&
           request.status == 201
         ) {
-          const response = JSON.parse(request.responseText);
-          this.dispatch("joinGame", response["game_id"]);
+          const { game_id } = JSON.parse(request.responseText);
+          commit("setGameId", game_id);
+          dispatch("joinGame", game_id);
         }
       });
       request.send(null);
     },
-    joinGame({ commit }, game_id) {
+    joinGame({ commit, dispatch }, game_id) {
       let request = new XMLHttpRequest();
       request.open(
         "POST",
@@ -45,6 +53,27 @@ export default new Vuex.Store({
           const response = JSON.parse(request.responseText);
           commit("setPlayer", response["player"]);
           commit("setAuthToken", response["X-Auth"]);
+          dispatch("getGameInfo");
+        }
+      });
+      request.send();
+    },
+    getGameInfo({ state, commit }) {
+      let request = new XMLHttpRequest();
+      request.open(
+        "GET",
+        "https://battleship-server-php.herokuapp.com/games/" +
+          state.game.game_id +
+          "/current"
+      );
+      request.setRequestHeader("X-Auth", state.authToken);
+      request.addEventListener("readystatechange", () => {
+        if (
+          request.readyState == XMLHttpRequest.DONE &&
+          request.status == 200
+        ) {
+          const game = JSON.parse(request.responseText);
+          commit("setGame", game);
         }
       });
       request.send();
