74ad7886161937a1fc794a44a5e59b946bdb86d7
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-05-06 22:57:07 +0200

    Improved Grid component:  - Added row and column's labels

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="grid">
-    <div v-for="row in grid" :key="row.index" class="line">
+  <div class="outer-box">
+    <div class="top-label">
+      <div class="label"></div>
       <div
-        v-for="cell in row.cells"
-        :key="cell.index"
-        class="cell"
-        @click="onClick(row.index, cell.index)"
+        v-for="(letter, index) in [
+          'A',
+          'B',
+          'C',
+          'D',
+          'E',
+          'F',
+          'G',
+          'H',
+          'I',
+          'J'
+        ]"
+        :key="index"
+        class="label"
       >
-        <div v-if="cell.striked === 'hit'">Hit</div>
-        <div v-else-if="cell.striked === 'missed'">miss</div>
-        <div v-else>?</div>
+        {{ letter }}
+      </div>
+    </div>
+    <div class="inner-box">
+      <div class="side-label">
+        <div v-for="index in 10" :key="index" class="label">
+          {{ index }}
+        </div>
+      </div>
+      <div class="grid">
+        <div v-for="row in grid" :key="row.index" class="line">
+          <div
+            v-for="cell in row.cells"
+            :key="cell.index"
+            class="cell"
+            @click="onClick(row.index, cell.index)"
+          >
+            <div v-if="cell.striked === 'hit'">Hit</div>
+            <div v-else-if="cell.striked === 'missed'">miss</div>
+            <div v-else>?</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -33,12 +63,11 @@ export default {
         });
       }
     }
-    console.log(this.$data.grid);
+    console.debug(this.$data.grid);
   },
   methods: {
     onClick(row, cell) {
-      console.log([row, cell]);
-      this.grid[row].cells[cell].striked = "hit";
+      this.$emit("cellClicked", row, cell);
     }
   }
 };
@@ -52,6 +81,7 @@ export default {
   border: 0.5px dashed #061a40;
   flex: 1;
 }
+
 .line {
   display: flex;
   flex: 1;
@@ -59,8 +89,33 @@ export default {
 .grid {
   display: flex;
   flex-direction: column;
+  background-color: #0353a4;
+  flex: 10;
+}
+
+.outer-box {
+  display: flex;
+  flex-direction: column;
   height: 500px;
   width: 500px;
-  background-color: #0353a4;
+}
+.inner-box {
+  display: flex;
+  flex: 10;
+}
+.top-label {
+  display: flex;
+  flex: 1;
+}
+.side-label {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+.label {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
 }
 </style>
