c040855e6a1018a4fbef0c93f21dd6ab6ef30e60
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-05-18 16:05:31 +0200

    Added ship rotation

--- src/views/PlaceShips.vue
+++ src/views/PlaceShips.vue
@@ -2,14 +2,16 @@
   <main>
     <h3>Game ID: {{ gameId }}</h3>
     <v-row justify="space-around">
-      <grid />
-      <v-radio-group v-model="selectedShip">
-        <v-radio
-          v-for="(ship, index) in ships"
-          :label="ship.name + ' orientation: ' + ship.orientation"
-          :key="index"
-          :value="index"
-        />
+      <grid :boats="ships" @cellClicked="onCellClicked" />
+      <v-radio-group v-model="selectedShip" class="column">
+        <div v-for="(ship, index) in ships" :key="index" class="d-flex">
+          <v-radio
+            :label="ship.name + ' orientation: ' + ship.orientation"
+            :value="index"
+          />
+          <v-btn @click="rotateLeft(index)">Rotate left</v-btn>
+          <v-btn @click="rotateRight(index)">Rotate right</v-btn>
+        </div>
       </v-radio-group>
     </v-row>
   </main>
@@ -28,7 +30,7 @@ export default {
         { name: "Battleship", size: 4, positions: [], orientation: "north" },
         { name: "Patrol Boat", size: 2, positions: [], orientation: "north" }
       ],
-      selectedShip: Number
+      selectedShip: 0
     };
   },
   components: {
@@ -37,6 +39,73 @@ export default {
   computed: {
     gameId() {
       return this.$store.state.game.game_id;
+    }
+  },
+  methods: {
+    rotateLeft(index) {
+      switch (this.ships[index].orientation) {
+        case "north":
+          this.ships[index].orientation = "west";
+          break;
+        case "west":
+          this.ships[index].orientation = "south";
+          break;
+        case "south":
+          this.ships[index].orientation = "est";
+          break;
+        case "est":
+          this.ships[index].orientation = "north";
+          break;
+      }
+    },
+    rotateRight(index) {
+      switch (this.ships[index].orientation) {
+        case "north":
+          this.ships[index].orientation = "est";
+          break;
+        case "west":
+          this.ships[index].orientation = "north";
+          break;
+        case "south":
+          this.ships[index].orientation = "west";
+          break;
+        case "est":
+          this.ships[index].orientation = "south";
+          break;
+      }
+    },
+    onCellClicked(row, cell) {
+      console.debug("Cell clicked: ( " + row + ", " + cell + " )");
+      const ship = this.ships[this.selectedShip];
+      let x = 0;
+      let y = 0;
+      switch (ship.orientation) {
+        case "north":
+          x = 1;
+          break;
+        case "west":
+          y = 1;
+          break;
+        case "south":
+          x = -1;
+          break;
+        case "est":
+          y = -1;
+          break;
+      }
+
+      if (
+        row + (ship.size - 1) * x < 10 &&
+        row + (ship.size - 1) * x >= 0 &&
+        cell + (ship.size - 1) * y < 10 &&
+        cell + (ship.size - 1) * y >= 0
+      ) {
+        ship.positions = [];
+        for (let i = 0; i < ship.size; i++) {
+          ship.positions.push([row + x * i, cell + y * i]);
+        }
+        console.debug(ship.positions);
+      }
     }
   }
 };
@@ -48,7 +117,7 @@ main {
 }
 h3 {
   font-size: 2vw;
-  color: #061a40;
+  color: var(--v-primary-base);
   text-align: center;
 }
 </style>
